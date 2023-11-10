6b6ca75d0dde88c95a5a9ebea6fdecf0ea8ca9f9
V9-9 <148236657+V9-9@users.noreply.github.com>
2023-11-10 15:57:50 +0300

    dz2

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,86 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
-/* Д/з Напишите метод, который добавляет 500000 элементов в ArrayList и LinkedList
-и измерьте время, которое потрачено на это. Сравните результаты.  */
+/* Д/з Расширим предыдущую задачу. Замерить время добавления, поиска и удаления
+объектов из коллекции в LinkedList, ArrayList, TreeSet, HashSet. Сравнить время
+и сделать вывод о том, в каких условиях какая коллекция работает быстрее.  */
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+        Set<Integer> treeSet = new TreeSet<>();
+        Set<Integer> hashSet = new HashSet<>();
 
-        long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 500000; i++){
-            arrayList.add(i);
+        System.out.println("Время добавления элементов в ArrayList: " + addElements(arrayList));
+        System.out.println("Время добавления элементов в LinkedList: " + addElements(linkedList));
+        System.out.println("Время добавления элементов в TreeSet: " + addElements(treeSet));
+        System.out.println("Время добавления элементов в HashSet: " + addElements(hashSet));
+
+        System.out.println("Время поиска элементов в ArrayList: " + searchElements(arrayList));
+        System.out.println("Время поиска элементов в LinkedList: " + searchElements(linkedList));
+        System.out.println("Время поиска элементов в TreeSet: " + searchElements(treeSet));
+        System.out.println("Время поиска элементов в HashSet: " + searchElements(hashSet));
+
+        System.out.println("Время удаления элементов в ArrayList: " + removeElements(arrayList));
+        System.out.println("Время удаления элементов в LinkedList: " + removeElements(linkedList));
+        System.out.println("Время удаления элементов в TreeSet: " + removeElements(treeSet));
+        System.out.println("Время удаления элементов в HashSet: " + removeElements(hashSet));
+    }
+
+    public static long addElements(List<Integer> list){
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 500_000; i++){
+            list.add(i);
         }
-        long endTime = System.currentTimeMillis();
-        long arrayListTime = endTime - startTime;
+        time = System.currentTimeMillis() - time;
+        return time;
+    }
 
-        startTime = System.currentTimeMillis();
-        for (int i = 0; i < 500000; i++){
-            linkedList.add(i);
+    public static long addElements(Set<Integer> set){
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 500_000; i++){
+            set.add(i);
         }
-        endTime = System.currentTimeMillis();
-        long linkedListTime = endTime - startTime;
+        time = System.currentTimeMillis() - time;
+        return time;
+    }
 
-        if (arrayListTime > linkedListTime){
-            System.out.println("\nМеньшее количество времени потрачено на добавление элементов в LinkedList");
-        } else if (arrayListTime < linkedListTime){
-            System.out.println("\nМеньшее количество времени потрачено на добавление элементов в ArrayList");
-        } else {
-            System.out.println("\nОдинаковое количество времени ушло на добавление элементов в оба класса");
+    public static long searchElements(List<Integer> list){
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 500_000; i++){
+            list.contains(i);
         }
+        time = System.currentTimeMillis() - time;
+        return time;
+    }
 
-        System.out.println("Время добавления элементов в ArrayList: " + arrayListTime);
-        System.out.println("Время добавления элементов в LinkedList: " + linkedListTime);
+    public static long searchElements(Set<Integer> set){
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 500_000; i++){
+            set.contains(i);
+        }
+        time = System.currentTimeMillis() - time;
+        return time;
+    }
 
+    public static long removeElements(List<Integer> list){
+        long time = System.currentTimeMillis();
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+        }
+        time = System.currentTimeMillis() - time;
+        return time;
+    }
+
+    public static long removeElements(Set<Integer> set){
+        long time = System.currentTimeMillis();
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+        }
+        time = System.currentTimeMillis() - time;
+        return time;
     }
 }
